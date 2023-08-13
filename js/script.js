a47287919d103b57b36b6ba4e70363a42724a360
kallolpandit <kallolpandit466@gmail.com>
2023-08-13 20:30:03 +0600

    banner

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(".banner_slide").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrow: true,
+  dots: true,
+  prevArrow: '<i class="fa-solid fa-chevron-left pre_arrow"></i>',
+  nextArrow: '<i class="fa-solid fa-chevron-right next_arrow"></i>',
+});
