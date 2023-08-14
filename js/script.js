d2aa1c1d7d5fb2e5526b6eb56e4a23a16503cdd4
kallolpandit <kallolpandit466@gmail.com>
2023-08-14 23:56:52 +0600

    sale

--- js/script.js
+++ js/script.js
@@ -8,3 +8,14 @@ $(".banner_slide").slick({
   prevArrow: '<i class="fa-solid fa-chevron-left pre_arrow"></i>',
   nextArrow: '<i class="fa-solid fa-chevron-right next_arrow"></i>',
 });
+// ==========================Banner Slide End=========================
+$(".sale_slide").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrow: true,
+  dots: false,
+  prevArrow: '<i class="fa-solid fa-chevron-left pre_arrow"></i>',
+  nextArrow: '<i class="fa-solid fa-chevron-right next_arrow"></i>',
+});
